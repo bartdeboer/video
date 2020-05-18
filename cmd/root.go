d9cebbf749d2ce5c1db928d7d8685b95c77365a7
Bart de Boer <bart.deboer@gmail.com>
2020-05-18 12:30:18 +0200

    Correct copyright year

--- cmd/root.go
+++ cmd/root.go
@@ -1,4 +1,4 @@
-// Copyright 2009 Bart de Boer. All rights reserved.
+// Copyright 2020 Bart de Boer. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
